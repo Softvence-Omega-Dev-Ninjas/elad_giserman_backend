6aded2537e26c6cfeac129eedf7824f1bbf0993a
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-14 04:55:58 +0600

    feat: get plan for user endpoint added

--- src/main/user/subscription/subscription.controller.ts
+++ src/main/user/subscription/subscription.controller.ts
@@ -2,17 +2,23 @@ import { GetUser, ValidateAuth } from '@/common/jwt/jwt.decorator';
 import { Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateIntentService } from './services/create-intent.service';
+import { SubscriptionService } from './services/subscription.service';
 
 @ApiTags('User -- Subscription')
 @ApiBearerAuth()
 @ValidateAuth()
 @Controller('subscription')
 export class SubscriptionController {
-  constructor(private readonly createIntentService: CreateIntentService) {}
+  constructor(
+    private readonly subscriptionService: SubscriptionService,
+    private readonly createIntentService: CreateIntentService,
+  ) {}
 
   @ApiOperation({ summary: 'Get plans for user' })
   @Get()
-  async getPlansForUser() {}
+  async getPlansForUser() {
+    return this.subscriptionService.getPlansForUser();
+  }
 
   @ApiOperation({ summary: 'Create payment intent' })
   @Post(':planId')
--- src/main/user/subscription/subscription.module.ts
+++ src/main/user/subscription/subscription.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CreateIntentService } from './services/create-intent.service';
 import { SubscriptionController } from './subscription.controller';
+import { SubscriptionService } from './services/subscription.service';
 
 @Module({
   controllers: [SubscriptionController],
-  providers: [CreateIntentService],
+  providers: [CreateIntentService, SubscriptionService],
 })
 export class SubscriptionModule {}
