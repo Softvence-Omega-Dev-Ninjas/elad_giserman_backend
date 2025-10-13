40934b1a75fc79199981c9468f1dfa56004177bf
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-14 04:33:40 +0600

    feat: create payment intent successfull

--- src/main/user/subscription/subscription.controller.ts
+++ src/main/user/subscription/subscription.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { GetUser, ValidateAuth } from '@/common/jwt/jwt.decorator';
+import { Controller, Param, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { SubscriptionService } from './services/subscription.service';
 
+@ApiTags('User -- Subscription')
+@ApiBearerAuth()
+@ValidateAuth()
 @Controller('subscription')
-export class SubscriptionController {}
+export class SubscriptionController {
+  constructor(private readonly subscriptionService: SubscriptionService) {}
+
+  @ApiOperation({ summary: 'Create payment intent' })
+  @Post(':planId')
+  async createPaymentIntent(
+    @GetUser('sub') userId: string,
+    @Param('planId') planId: string,
+  ) {
+    return this.subscriptionService.createPaymentIntent(userId, planId);
+  }
+}
