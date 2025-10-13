5e5322937308f37a49c8b1dc5e5761dd13c8211a
shahadathhs <shahadathhossensajib732@gmail.com>
2025-10-13 06:29:46 +0600

    feat: update nodemailer setup for render

--- src/lib/mail/mail.service.ts
+++ src/lib/mail/mail.service.ts
@@ -1,6 +1,7 @@
+import { ENVEnum } from '@/common/enum/env.enum';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ENVEnum } from '@/common/enum/env.enum';
+import { google } from 'googleapis';
 import * as nodemailer from 'nodemailer';
 
 @Injectable()
@@ -8,14 +9,44 @@ export class MailService {
   private transporter: nodemailer.Transporter;
   private fromEmail: string;
 
+  // constructor(private configService: ConfigService) {
+  //   const user = this.configService.getOrThrow<string>(ENVEnum.MAIL_USER);
+  //   const pass = this.configService.getOrThrow<string>(ENVEnum.MAIL_PASS);
+
+  //   this.fromEmail = user;
+  //   this.transporter = nodemailer.createTransport({
+  //     service: 'gmail',
+  //     auth: { user, pass },
+  //   });
+  // }
   constructor(private configService: ConfigService) {
-    const user = this.configService.getOrThrow<string>(ENVEnum.MAIL_USER);
-    const pass = this.configService.getOrThrow<string>(ENVEnum.MAIL_PASS);
+    const clientId = this.configService.getOrThrow<string>(
+      ENVEnum.OAUTH_CLIENT_ID,
+    );
+    const clientSecret = this.configService.getOrThrow<string>(
+      ENVEnum.OAUTH_CLIENT_SECRET,
+    );
+    const refreshToken = this.configService.getOrThrow<string>(
+      ENVEnum.OAUTH_REFRESH_TOKEN,
+    );
+    const user = this.configService.getOrThrow<string>(
+      ENVEnum.OAUTH_GMAIL_USER,
+    );
 
     this.fromEmail = user;
+
+    const oauth2Client = new google.auth.OAuth2(clientId, clientSecret);
+    oauth2Client.setCredentials({ refresh_token: refreshToken });
+
     this.transporter = nodemailer.createTransport({
       service: 'gmail',
-      auth: { user, pass },
+      auth: {
+        type: 'OAuth2',
+        user,
+        clientId,
+        clientSecret,
+        refreshToken,
+      },
     });
   }
 
