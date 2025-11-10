d7d854e8327b223bd91c9afcf6f684691b6dd04d
linux44gh6 <abireshan32@gmail.com>
2025-11-11 04:36:50 +0600

    admin reviews data

--- src/main/admin/admin.module.ts
+++ src/main/admin/admin.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { AdminOfferService } from './service/admin-offer.service';
 import { AdminController } from './controller/admin.controller';
+import { AdminReviewController } from './controller/admin.review.controller';
+import { AdminReviewService } from './service/admin-review.service';
 
 @Module({
   imports: [SubscriptionModule],
-  providers: [AdminOfferService],
-  controllers: [AdminController],
+  providers: [AdminOfferService,AdminReviewService],
+  controllers: [AdminController,AdminReviewController],
 })
 export class AdminModule {}
