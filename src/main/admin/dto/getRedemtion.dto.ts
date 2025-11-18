2f2b8b0870006e8a7e99bd4efbd19a8ec895a0c0
linux44gh6 <abireshan32@gmail.com>
2025-11-19 03:08:44 +0600

    fix code

--- src/main/admin/dto/getRedemtion.dto.ts
+++ src/main/admin/dto/getRedemtion.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional, IsString, IsInt } from 'class-validator';
+import { IsOptional, IsInt } from 'class-validator';
 import { Type } from 'class-transformer';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
