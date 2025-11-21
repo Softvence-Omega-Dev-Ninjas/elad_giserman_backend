b210629b1b87c913004685e8fac7b2042e3013cd
linux44gh6 <abireshan32@gmail.com>
2025-11-22 05:20:51 +0600

    ✨ feat: Remove `IsEnum` validator from getReview.dto.ts
    The `IsEnum` validator from `class-validator` and its usage were removed
    from the `getReview.dto.ts` file as it's no longer required.

--- src/main/admin/dto/getReview.dto.ts
+++ src/main/admin/dto/getReview.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional, IsString, IsInt, IsEnum } from 'class-validator';
+import { IsOptional, IsString, IsInt } from 'class-validator';
 import { Type } from 'class-transformer';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
